d0ffaafc3191b07e2fd470c32ec8df0c2e0e976a
Gabriel Tan-Chen <gabriel@gabrieltanchen.com>
2019-09-12 08:00:42 -0400

    Create index route

---
+++ app/routes/index.js
@@ -0,0 +1,8 @@
+// import ENV from 'annual-report-2019/config/environment';
+// import { get, set } from '@ember/object';
+import Route from '@ember/routing/route';
+
+export default Route.extend({
+  afterModel() {
+  },
+});
