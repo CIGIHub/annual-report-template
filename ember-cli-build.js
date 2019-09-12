50c1cc47aa4026877e6c9d774825591aeea56c66
Gabriel Tan-Chen <gabriel@gabrieltanchen.com>
2019-09-12 11:07:55 -0400

    Add favicon config

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,7 +4,19 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   const app = new EmberApp(defaults, {
-    // Add options here
+    'ember-cli-favicon': {
+      enabled: process.env.EMBER_ENV !== 'test',
+      faviconsConfig: {
+        appName: '2019 CIGI Annual Report',
+        developerName: 'Centre for International Governance Innovation',
+        developerURL: 'https://www.cigionline.org',
+        icons: {
+          coast: false,
+        },
+        path: process.env.EMBER_ENV === 'development' ? '/' : '/interactives/2019annualreport/assets/favicon',
+      },
+      iconPath: 'favicon.png',
+    },
   });
 
   app.import('node_modules/foundation-sites/dist/css/foundation.css');
