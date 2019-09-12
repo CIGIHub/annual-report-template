375ad5ab687dee91ce6bd1cf6794fee31e18700e
Gabriel Tan-Chen <gabriel@gabrieltanchen.com>
2019-09-12 18:16:10 -0400

    Table of Contents

--- app/routes/table-of-contents.js
+++ app/routes/table-of-contents.js
@@ -1,4 +1,13 @@
+import ENV from 'annual-report-2019/config/environment';
+import { set } from '@ember/object';
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 export default Route.extend({
+  headData: service(),
+  intl: service(),
+
+  afterModel() {
+    set(this, 'headData.url', `${ENV.host}${ENV.rootURL}table-of-contents/`);
+  },
 });
--- tests/unit/components/top-bar-test.js
+++ tests/unit/components/top-bar-test.js
@@ -410,4 +410,25 @@ module('Unit | Component | top-bar', function(hooks) {
       'should have correct twitterShareLink',
     );
   });
+
+  test('should have correct share links for /table-of-contents', function(assert) {
+    const component = this.owner.factoryFor('component:top-bar').create();
+    set(component, 'router.currentRouteName', 'table-of-contents');
+
+    assert.strictEqual(
+      get(component, 'shareRoute'),
+      'https://www.cigionline.org/interactives/2019annualreport/table-of-contents',
+      'should have correct shareRoute',
+    );
+    assert.strictEqual(
+      get(component, 'linkedInShareLink'),
+      'https://www.linkedin.com/shareArticle?mini=true&url=https://www.cigionline.org/interactives/2019annualreport/table-of-contents',
+      'should have correct linkedInShareLink',
+    );
+    assert.strictEqual(
+      get(component, 'twitterShareLink'),
+      'https://twitter.com/intent/tweet?status=2019+CIGI+Annual+Report+https://www.cigionline.org/interactives/2019annualreport/table-of-contents',
+      'should have correct twitterShareLink',
+    );
+  });
 });
