bc438fc098e591f40972e4ee8eba029a31368876
Gabriel Tan-Chen <gabriel@gabrieltanchen.com>
2019-09-12 09:02:35 -0400

    Set default title

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,11 +1,21 @@
+import ENV from 'annual-report-2019/config/environment';
+import { get, set } from '@ember/object';
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 
 export default Route.extend({
+  headData: service(),
   intl: service(),
 
   beforeModel(...args) {
     this._super(args);
     return this.intl.setLocale('en-ca');
+  },
+
+  afterModel() {
+    set(this, 'headData.title', get(this, 'intl').t('title'));
+    set(this, 'headData.siteName', get(this, 'intl').t('title'));
+    set(this, 'headData.description', get(this, 'intl').t('description'));
+    set(this, 'headData.url', `${ENV.host}${ENV.rootURL}`);
   },
 });
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,8 +1,4 @@
-// import ENV from 'annual-report-2019/config/environment';
-// import { get, set } from '@ember/object';
 import Route from '@ember/routing/route';
 
 export default Route.extend({
-  afterModel() {
-  },
 });
