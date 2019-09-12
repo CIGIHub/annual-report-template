ffe55c8f4edbfc3f6486be92df383b51085b3feb
Gabriel Tan-Chen <gabriel@gabrieltanchen.com>
2019-09-12 16:50:40 -0400

    Chair's Message

--- tests/unit/components/top-bar-test.js
+++ tests/unit/components/top-bar-test.js
@@ -158,4 +158,25 @@ module('Unit | Component | top-bar', function(hooks) {
       'should have correct twitterShareLink',
     );
   });
+
+  test('should have correct share links for /chairs-message', function(assert) {
+    const component = this.owner.factoryFor('component:top-bar').create();
+    set(component, 'router.currentRouteName', 'chairs-message');
+
+    assert.strictEqual(
+      get(component, 'shareRoute'),
+      'https://www.cigionline.org/interactives/2019annualreport/chairs-message',
+      'should have correct shareRoute',
+    );
+    assert.strictEqual(
+      get(component, 'linkedInShareLink'),
+      'https://www.linkedin.com/shareArticle?mini=true&url=https://www.cigionline.org/interactives/2019annualreport/chairs-message',
+      'should have correct linkedInShareLink',
+    );
+    assert.strictEqual(
+      get(component, 'twitterShareLink'),
+      'https://twitter.com/intent/tweet?status=2019+CIGI+Annual+Report+https://www.cigionline.org/interactives/2019annualreport/chairs-message',
+      'should have correct twitterShareLink',
+    );
+  });
 });
