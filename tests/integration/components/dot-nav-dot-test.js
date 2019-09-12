d6c669bbc99a021c53284042f0efb5560547a0c4
Gabriel Tan-Chen <gabriel@gabrieltanchen.com>
2019-09-12 18:32:55 -0400

    create components

---
+++ tests/integration/components/dot-nav-dot-test.js
@@ -0,0 +1,17 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import hbs from 'htmlbars-inline-precompile';
+
+module('Integration | Component | dot-nav-dot', function(hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders', async function(assert) {
+    // Set any properties with this.set('myProperty', 'value');
+    // Handle any actions with this.set('myAction', function(val) { ... });
+
+    await render(hbs`<DotNavDot />`);
+
+    assert.ok(this.element.querySelector('.link-item'));
+  });
+});
