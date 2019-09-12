b49cb8a10c7d27af294369a7e34823c6bcc43a86
Gabriel Tan-Chen <gabriel@gabrieltanchen.com>
2019-09-12 09:36:47 -0400

    Add acceptance tests

--- config/environment.js
+++ config/environment.js
@@ -2,7 +2,20 @@
 
 module.exports = function(environment) {
   const ENV = {
-    modulePrefix: 'annual-report-2019',
+    APP: {
+      // Here you can pass flags/options to your application instance
+      // when it is created
+    },
+    EmberENV: {
+      FEATURES: {
+        // Here you can enable experimental features on an ember canary build
+        // e.g. EMBER_NATIVE_DECORATOR_SUPPORT: true
+      },
+      EXTEND_PROTOTYPES: {
+        // Prevent Ember Data from overriding Date.parse.
+        Date: false,
+      },
+    },
     environment,
     fontawesome: {
       icons: {
@@ -31,31 +44,15 @@ module.exports = function(environment) {
         ],
       },
     },
-    rootURL: '/',
+    host: 'https://www.cigionline.org',
     locationType: 'auto',
-    EmberENV: {
-      FEATURES: {
-        // Here you can enable experimental features on an ember canary build
-        // e.g. EMBER_NATIVE_DECORATOR_SUPPORT: true
-      },
-      EXTEND_PROTOTYPES: {
-        // Prevent Ember Data from overriding Date.parse.
-        Date: false,
-      },
-    },
-
-    APP: {
-      // Here you can pass flags/options to your application instance
-      // when it is created
-    },
+    modulePrefix: 'annual-report-2019',
+    rootURL: '/interactives/2019annualreport/',
   };
 
   if (environment === 'development') {
-    // ENV.APP.LOG_RESOLVER = true;
-    // ENV.APP.LOG_ACTIVE_GENERATION = true;
-    // ENV.APP.LOG_TRANSITIONS = true;
-    // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
-    // ENV.APP.LOG_VIEW_LOOKUPS = true;
+    ENV.host = 'http://localhost:4200';
+    ENV.rootURL = '/';
   }
 
   if (environment === 'test') {
@@ -71,7 +68,9 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-    // here you can enable a production-specific feature
+    if (process.env.STAGING) {
+      ENV.host = 'https://staging.cigionline.org';
+    }
   }
 
   return ENV;
