edf931e000aadc0d5a195967261c48197c0aa907
Gabriel Tan-Chen <gabriel@gabrieltanchen.com>
2019-09-12 08:42:44 -0400

    Stub intl service

--- tests/unit/routes/application-test.js
+++ tests/unit/routes/application-test.js
@@ -1,8 +1,20 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
+import Service from '@ember/service';
+
+const intlStub = Service.extend({
+  locale: null,
+  setLocale(locale) {
+    this.set('locale', locale);
+  },
+});
 
 module('Unit | Route | application', function(hooks) {
   setupTest(hooks);
+
+  hooks.beforeEach(function() {
+    this.owner.register('service:intl', intlStub);
+  });
 
   test('it exists', function(assert) {
     const route = this.owner.lookup('route:application');
