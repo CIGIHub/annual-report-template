a27d63e6f96923da221a7ed074bec3034a392652
Gabriel Tan-Chen <gabriel@gabrieltanchen.com>
2019-09-12 16:45:48 -0400

    Building Closer Ties between Canada and India

--- app/routes/building-closer-ties-between-canada-and-india.js
+++ app/routes/building-closer-ties-between-canada-and-india.js
@@ -1,4 +1,16 @@
+import ENV from 'annual-report-2019/config/environment';
+import { get, set } from '@ember/object';
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 export default Route.extend({
+  headData: service(),
+  intl: service(),
+
+  afterModel() {
+    const title = `${get(this, 'intl').t('buildingCloserTiesBetweenCanadaAndIndia.title')} | ${get(this, 'intl').t('title')}`;
+    set(this, 'headData.title', title);
+    set(this, 'headData.description', get(this, 'intl').t('buildingCloserTiesBetweenCanadaAndIndia.description'));
+    set(this, 'headData.url', `${ENV.host}${ENV.rootURL}building-closer-ties-between-canada-and-india/`);
+  },
 });
--- tests/unit/components/top-bar-test.js
+++ tests/unit/components/top-bar-test.js
@@ -137,4 +137,25 @@ module('Unit | Component | top-bar', function(hooks) {
       'should have correct twitterShareLink',
     );
   });
+
+  test('should have correct share links for /building-closer-ties-between-canada-and-india', function(assert) {
+    const component = this.owner.factoryFor('component:top-bar').create();
+    set(component, 'router.currentRouteName', 'building-closer-ties-between-canada-and-india');
+
+    assert.strictEqual(
+      get(component, 'shareRoute'),
+      'https://www.cigionline.org/interactives/2019annualreport/building-closer-ties-between-canada-and-india',
+      'should have correct shareRoute',
+    );
+    assert.strictEqual(
+      get(component, 'linkedInShareLink'),
+      'https://www.linkedin.com/shareArticle?mini=true&url=https://www.cigionline.org/interactives/2019annualreport/building-closer-ties-between-canada-and-india',
+      'should have correct linkedInShareLink',
+    );
+    assert.strictEqual(
+      get(component, 'twitterShareLink'),
+      'https://twitter.com/intent/tweet?status=2019+CIGI+Annual+Report+https://www.cigionline.org/interactives/2019annualreport/building-closer-ties-between-canada-and-india',
+      'should have correct twitterShareLink',
+    );
+  });
 });
