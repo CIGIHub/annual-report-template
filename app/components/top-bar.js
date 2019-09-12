9fe4878d7c2fd3e51969b6e885861d4a9b031e7b
Gabriel Tan-Chen <gabriel@gabrieltanchen.com>
2019-09-12 11:53:27 -0400

    test menuIsOpen

--- app/components/top-bar.js
+++ app/components/top-bar.js
@@ -1,13 +1,20 @@
 import ENV from 'annual-report-2019/config/environment';
 import Component from '@ember/component';
 import { computed, get } from '@ember/object';
+import { inject as service } from '@ember/service';
 
 export default Component.extend({
+  lightbox: service(),
+
   hideMenu: computed('currentRoute', function() {
     if (get(this, 'currentRoute') === 'table-of-contents') {
       return true;
     }
     return false;
+  }),
+
+  menuIsOpen: computed('lightbox.showLightbox', function() {
+    return get(this, 'lightbox.showLightbox') === 'tableofcontents';
   }),
 
   shareRoute: computed('currentRoute', function() {
--- tests/unit/components/top-bar-test.js
+++ tests/unit/components/top-bar-test.js
@@ -1,28 +1,55 @@
+import { get, set } from '@ember/object';
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
+import Service from '@ember/service';
 
 module('Unit | Component | top-bar', function(hooks) {
   setupTest(hooks);
 
+  hooks.beforeEach(function() {
+    this.owner.register('service:lightbox', Service.extend({
+      showLightbox: null,
+    }));
+  });
+
   test('should hide menu when on table of contents route', function(assert) {
     const component = this.owner.factoryFor('component:top-bar').create();
+    set(component, 'currentRoute', 'table-of-contents');
 
-    component.set('currentRoute', 'table-of-contents');
-
-    assert.strictEqual(component.get('hideMenu'), true);
+    assert.strictEqual(get(component, 'hideMenu'), true);
   });
 
   test('should not hide menu on any other route', function(assert) {
     const component = this.owner.factoryFor('component:top-bar').create();
 
-    assert.strictEqual(component.get('hideMenu'), false);
+    assert.strictEqual(get(component, 'hideMenu'), false);
+  });
+
+  test('should have menuIsOpen=false with no lightbox open', function(assert) {
+    const component = this.owner.factoryFor('component:top-bar').create();
+    set(component, 'lightbox.showLightbox', null);
+
+    assert.strictEqual(get(component, 'menuIsOpen'), false);
+  });
+
+  test('should have menuIsOpen=true with table of contents lightbox', function(assert) {
+    const component = this.owner.factoryFor('component:top-bar').create();
+    set(component, 'lightbox.showLightbox', 'tableofcontents');
+
+    assert.strictEqual(get(component, 'menuIsOpen'), true);
+  });
+
+  test('should have menuIsOpen=false with another lightbox type', function(assert) {
+    const component = this.owner.factoryFor('component:top-bar').create();
+    set(component, 'lightbox.showLightbox', 'youtube');
+
+    assert.strictEqual(get(component, 'menuIsOpen'), false);
   });
 
   test('should have correct share route for /', function(assert) {
     const component = this.owner.factoryFor('component:top-bar').create();
+    set(component, 'currentRoute', 'index');
 
-    component.set('currentRoute', 'index');
-
-    assert.strictEqual(component.get('shareRoute'), 'https://www.cigionline.org/interactives/2019annualreport/');
+    assert.strictEqual(get(component, 'shareRoute'), 'https://www.cigionline.org/interactives/2019annualreport/');
   });
 });
