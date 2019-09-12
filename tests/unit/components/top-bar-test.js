a03122ecb7910b270fbff605e6973481315ba36a
Gabriel Tan-Chen <gabriel@gabrieltanchen.com>
2019-09-12 18:21:31 -0400

    Explore Timeline

--- tests/unit/components/top-bar-test.js
+++ tests/unit/components/top-bar-test.js
@@ -452,4 +452,25 @@ module('Unit | Component | top-bar', function(hooks) {
       'should have correct twitterShareLink',
     );
   });
+
+  test('should have correct share links for /timeline', function(assert) {
+    const component = this.owner.factoryFor('component:top-bar').create();
+    set(component, 'router.currentRouteName', 'timeline');
+
+    assert.strictEqual(
+      get(component, 'shareRoute'),
+      'https://www.cigionline.org/interactives/2019annualreport/timeline',
+      'should have correct shareRoute',
+    );
+    assert.strictEqual(
+      get(component, 'linkedInShareLink'),
+      'https://www.linkedin.com/shareArticle?mini=true&url=https://www.cigionline.org/interactives/2019annualreport/timeline',
+      'should have correct linkedInShareLink',
+    );
+    assert.strictEqual(
+      get(component, 'twitterShareLink'),
+      'https://twitter.com/intent/tweet?status=2019+CIGI+Annual+Report+https://www.cigionline.org/interactives/2019annualreport/timeline',
+      'should have correct twitterShareLink',
+    );
+  });
 });
