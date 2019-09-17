4e759ff2449b579a87cbb8b8538c7dfa6a170dce
Gabriel Tan-Chen <gabriel@gabrieltanchen.com>
2019-09-17 07:51:45 -0400

    Add route image file

---
+++ app/route-image.js
@@ -0,0 +1,18 @@
+import ENV from 'annual-report-2019/config/environment';
+
+export default (routeName) => {
+  const backgroundImages = {
+    'chairs-message': '3E6r1xmCHg1kIA7w/chairs-message.jpg',
+  };
+
+  let host = 'https://www.cigionline.org';
+  if (ENV.environment !== 'production' || process.env.STAGING) {
+    host = 'https://staging.cigionline.org';
+  }
+
+  if (backgroundImages[routeName]) {
+    return `${host}/interactives/2019annualreport/static/${backgroundImages[routeName]}`;
+  }
+
+  return null;
+};
