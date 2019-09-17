753b175c70551c93367fc324d868c534b88ad2d8
Gabriel Tan-Chen <gabriel@gabrieltanchen.com>
2019-09-17 08:36:14 -0400

    unit and integration tests

--- tests/integration/components/footer-photo-test.js
+++ tests/integration/components/footer-photo-test.js
@@ -1,7 +1,8 @@
-import { module, test } from 'qunit';
+import { get, set } from '@ember/object';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { click, render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
+import { module, test } from 'qunit';
 
 module('Integration | Component | footer-photo', function(hooks) {
   setupRenderingTest(hooks);
@@ -10,5 +11,33 @@ module('Integration | Component | footer-photo', function(hooks) {
     await render(hbs`<FooterPhoto />`);
 
     assert.ok(this.element.querySelector('.footer'));
+  });
+
+  test('should set revealToggle=true on footer-icon-btn click when revealToggle=false', async function(assert) {
+    set(this, 'revealToggle', false);
+
+    await render(hbs`<FooterPhoto @revealToggle={{this.revealToggle}} />`);
+    assert.ok(
+      this.element.querySelector('.footer-icon-btn'),
+      'should find footer-icon-btn',
+    );
+
+    await click('.footer-icon-btn');
+
+    assert.strictEqual(get(this, 'revealToggle'), true);
+  });
+
+  test('should set revealToggle=false on footer-icon-btn click when revealToggle=true', async function(assert) {
+    set(this, 'revealToggle', true);
+
+    await render(hbs`<FooterPhoto @revealToggle={{this.revealToggle}} />`);
+    assert.ok(
+      this.element.querySelector('.footer-icon-btn'),
+      'should find footer-icon-btn',
+    );
+
+    await click('.footer-icon-btn');
+
+    assert.strictEqual(get(this, 'revealToggle'), false);
   });
 });
