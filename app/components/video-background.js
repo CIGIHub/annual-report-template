763c78f9085d94af024afa6ee13da74c6befde7a
Gabriel Tan-Chen <gabriel@gabrieltanchen.com>
2019-09-19 14:48:48 -0400

    fix test

--- app/components/video-background.js
+++ app/components/video-background.js
@@ -1,9 +1,10 @@
+import ENV from 'annual-report-2019/config/environment';
 import Component from '@ember/component';
 
 export default Component.extend({
   didInsertElement() {
     const video = document.getElementById('background-video');
-    if (video) {
+    if (video && ENV.environment !== 'test') {
       video.muted = true;
       video.play();
     }
