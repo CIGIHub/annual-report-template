02d1d071957840316fb3069793bc7d29b49430ab
Gabriel Tan-Chen <gabriel@gabrieltanchen.com>
2019-09-19 16:18:47 -0400

    Models for Data Governance slide

--- app/route-image.js
+++ app/route-image.js
@@ -8,6 +8,7 @@ export default (routeName) => {
     'chairs-message': '3E6r1xmCHg1kIA7w/chairs-message.jpg',
     'governing-cyberspace-during-a-crisis-in-trust': '3E6r1xmCHg1kIA7w/governing-cyberspace-during-a-crisis-in-trust.jpg',
     'media-and-mass-atrocity-the-rwanda-genocide-and-beyond': '3E6r1xmCHg1kIA7w/media-and-mass-atrocity-the-rwanda-genocide-and-beyond.jpg',
+    'models-for-data-governance': '3E6r1xmCHg1kIA7w/models-for-data-governance.jpg',
     'presidents-message': '3E6r1xmCHg1kIA7w/presidents-message.jpg',
   };
 
