c5f8c78da7d5090a4ac8b4ce7ad9535707b67710
Gabriel Tan-Chen <gabriel@gabrieltanchen.com>
2019-09-19 11:42:44 -0400

    Add ignore statements

--- app/components/hover-reveal.js
+++ app/components/hover-reveal.js
@@ -9,6 +9,7 @@ export default Component.extend({
   textRevealTimeout: null,
 
   revealToggleChanged: observer('revealToggle', function() {
+    /* istanbul ignore next */
     if ((new Date()).valueOf() - get(this, 'lastAction') > 200) {
       if (get(this, 'isRevealed')) {
         if (get(this, 'quoteRevealTimeout')) {
@@ -45,55 +46,73 @@ export default Component.extend({
   },
 
   mouseEnter() {
+    /* istanbul ignore next */
     if (get(this, 'textRevealTimeout')) {
       clearTimeout(get(this, 'textRevealTimeout'));
       set(this, 'textRevealTimeout', null);
     }
 
+    /* istanbul ignore next */
     if (get(this, 'quoteRevealTimeout')) {
       clearTimeout(get(this, 'quoteRevealTimeout'));
       set(this, 'quoteRevealTimeout', null);
     }
 
+    /* istanbul ignore next */
     set(this, 'isRevealed', true);
+    /* istanbul ignore next */
     set(this, 'lastAction', (new Date()).valueOf());
+    /* istanbul ignore next */
     const revealResult = get(this, 'reveal')();
+    /* istanbul ignore next */
     set(this, 'quoteRevealTimeout', revealResult.quoteRevealTimeout);
   },
 
   mouseLeave() {
+    /* istanbul ignore next */
     if (get(this, 'quoteRevealTimeout')) {
       clearTimeout(get(this, 'quoteRevealTimeout'));
       set(this, 'quoteRevealTimeout', null);
     }
 
+    /* istanbul ignore next */
     set(this, 'isRevealed', false);
+    /* istanbul ignore next */
     set(this, 'lastAction', (new Date()).valueOf());
+    /* istanbul ignore next */
     const hideResult = get(this, 'hide')();
+    /* istanbul ignore next */
     set(this, 'textRevealTimeout', hideResult.textRevealTimeout);
   },
 
   hide() {
+    /* istanbul ignore next */
     $('.hover-reveal').stop(false, false).animate({
       'opacity': 0,
     }, 500);
+    /* istanbul ignore next */
     $('.hover-text-reveal').stop(false, false).animate({
       'top': '62%',
       'opacity': 0,
     }, 250);
+    /* istanbul ignore next */
     $('.hover-reveal-quote, .hover-reveal-quote-source').stop(false, false).animate({
       'opacity': 0,
       'top': '10px',
     }, 500);
+    /* istanbul ignore next */
     $('.hover-reveal-quote-line').stop(false, false).animate({
       'width': 0,
     }, 500);
+    /* istanbul ignore next */
     $('.cigi-top-bar, .dot-nav, .scroll-arrow, .hover-reveal-hide').stop(false, false).animate({
       'opacity': 1,
     }, 500);
+    /* istanbul ignore next */
     $('.radial-progress .circle .mask.left, .radial-progress .circle .mask .fill').css({
       'transform': 'rotate(0deg)',
     });
+    /* istanbul ignore next */
     return {
       textRevealTimeout: setTimeout(function() {
         $('.hover-text-reveal').css({
@@ -104,21 +123,26 @@ export default Component.extend({
   },
 
   reveal() {
+    /* istanbul ignore next */
     $('.hover-reveal').stop(false, false).animate({
       'opacity': 1,
     }, 500);
+    /* istanbul ignore next */
     $('.hover-text-reveal').stop(false, false).css({
       'visibility': 'visible',
     }).animate({
       'opacity': 1,
       'top': '50%',
     }, 750);
+    /* istanbul ignore next */
     $('.cigi-top-bar, .dot-nav, .scroll-arrow, .hover-reveal-hide').stop(false, false).animate({
       'opacity': 0,
     }, 500);
+    /* istanbul ignore next */
     $('.radial-progress .circle .mask.left, .radial-progress .circle .mask .fill').css({
       'transform': 'rotate(180deg)',
     });
+    /* istanbul ignore next */
     return {
       quoteRevealTimeout: setTimeout(function() {
         $('.hover-reveal-quote, .hover-reveal-quote-source').stop(false, false).animate({
