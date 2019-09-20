f745aa00bd6ee2d693a6451f2d19941d80e18f02
Gabriel Tan-Chen <gabriel@gabrieltanchen.com>
2019-09-19 22:58:49 -0400

    add lightbox tests

--- tests/unit/services/lightbox-test.js
+++ tests/unit/services/lightbox-test.js
@@ -14,18 +14,67 @@ module('Unit | Service | lightbox', function(hooks) {
 
   test('should show table of contents lightbox', function(assert) {
     const service = this.owner.lookup('service:lightbox');
+    set(service, 'showLightbox', null);
+    set(service, 'subType', null);
 
     service.showTableOfContents();
 
     assert.strictEqual(get(service, 'showLightbox'), 'tableofcontents');
+    assert.strictEqual(get(service, 'subType'), null);
   });
 
   test('should close table of contents lightbox', function(assert) {
     const service = this.owner.lookup('service:lightbox');
     set(service, 'showLightbox', 'tableofcontents');
+    set(service, 'subType', null);
 
     service.closeLightbox();
 
     assert.strictEqual(get(service, 'showLightbox'), null);
+    assert.strictEqual(get(service, 'subType'), null);
+  });
+
+  test('should show table of contents acknowledgements', function(assert) {
+    const service = this.owner.lookup('service:lightbox');
+    set(service, 'showLightbox', 'tableofcontents');
+    set(service, 'subType', null);
+
+    service.showTableOfContentsAcknowledgements();
+
+    assert.strictEqual(get(service, 'showLightbox'), 'tableofcontents');
+    assert.strictEqual(get(service, 'subType'), 'acknowledgements');
+  });
+
+  test('should close table of contents acknowledgements', function(assert) {
+    const service = this.owner.lookup('service:lightbox');
+    set(service, 'showLightbox', 'tableofcontents');
+    set(service, 'subType', 'acknowledgements');
+
+    service.closeTableOfContentsAcknowledgements();
+
+    assert.strictEqual(get(service, 'showLightbox'), 'tableofcontents');
+    assert.strictEqual(get(service, 'subType'), null);
+  });
+
+  test('should show publication lightbox', function(assert) {
+    const service = this.owner.lookup('service:lightbox');
+    set(service, 'showLightbox', null);
+    set(service, 'subType', null);
+
+    service.showPublicationLightbox();
+
+    assert.strictEqual(get(service, 'showLightbox'), null);
+    assert.strictEqual(get(service, 'subType'), 'publication');
+  });
+
+  test('should close publication lightbox', function(assert) {
+    const service = this.owner.lookup('service:lightbox');
+    set(service, 'showLightbox', null);
+    set(service, 'subType', 'publication');
+
+    service.closeLightbox();
+
+    assert.strictEqual(get(service, 'showLightbox'), null);
+    assert.strictEqual(get(service, 'subType'), null);
   });
 });
