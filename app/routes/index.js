c453e9ee3760194fc912c91c22586f9e0703da36
Gabriel Tan-Chen <gabriel@gabrieltanchen.com>
2019-09-20 20:24:03 -0400

    extend routes

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,10 @@
+import GoogleAnalyticsMixin from 'annual-report-2019/mixins/google-analytics';
 import ResetScrollMixin from 'annual-report-2019/mixins/reset-scroll';
 import { get } from '@ember/object';
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 
-export default Route.extend(ResetScrollMixin, {
+export default Route.extend(GoogleAnalyticsMixin, ResetScrollMixin, {
   assetLoader: service(),
 
   beforeModel() {
