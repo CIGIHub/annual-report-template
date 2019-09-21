37cda360592790270daddcced5b5331f59b28844
Gabriel Tan-Chen <gabriel@gabrieltanchen.com>
2019-09-20 21:06:26 -0400

    Create lightbox-overlay component

---
+++ app/components/lightbox-overlay.js
@@ -0,0 +1,57 @@
+import Component from '@ember/component';
+import {
+  computed,
+  get,
+  observer,
+  set,
+} from '@ember/object';
+import { inject as service } from '@ember/service';
+import $ from 'jquery';
+
+import routeOrder from '../route-order';
+
+export default Component.extend({
+  lightbox: service(),
+
+  routes: routeOrder.slice(2),
+
+  showTableOfContents: computed('lightbox.showLightbox', function() {
+    return get(this, 'lightbox.showLightbox') === 'tableofcontents';
+  }),
+
+  showLightboxChanged: observer('lightbox.showLightbox', function() {
+    if (get(this, 'lightbox.showLightbox')) {
+      $('.lightbox').css({
+        'z-index': 5,
+      }).animate({
+        'opacity': 1,
+      }, 500);
+      $('.scroll-arrow').css({
+        'z-index': -1,
+      });
+      $('.dot-nav').css({
+        'z-index': -1,
+      });
+    } else {
+      $('.lightbox').animate({
+        'opacity': 0,
+      }, 500).css({
+        'z-index': -1,
+      });
+      $('.transition-row, .content-slide, .home-page, .chairs-message, .footer').animate({
+        'opacity': 1,
+      });
+      $('.scroll-arrow').css({
+        'z-index': 7,
+      });
+      $('.dot-nav').css({
+        'z-index': 1,
+      });
+    }
+  }),
+
+  init() {
+    set(this, 'lightbox.showLightbox', null);
+    return this._super();
+  },
+});
