0c8df611b6562b6e95c5d698baac098c7f8cfc26
Gabriel Tan-Chen <gabriel@gabrieltanchen.com>
2019-09-20 20:15:28 -0400

    Add ResetScrollMixin for transitions on mobile (#61)
    * create reset-scroll mixin
    
    * extend routes

--- app/routes/chairs-message.js
+++ app/routes/chairs-message.js
@@ -1,9 +1,10 @@
 import ENV from 'annual-report-2019/config/environment';
+import ResetScrollMixin from 'annual-report-2019/mixins/reset-scroll';
 import { get, set } from '@ember/object';
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 
-export default Route.extend({
+export default Route.extend(ResetScrollMixin, {
   assetLoader: service(),
   headData: service(),
   intl: service(),
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,8 +1,9 @@
+import ResetScrollMixin from 'annual-report-2019/mixins/reset-scroll';
 import { get } from '@ember/object';
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 
-export default Route.extend({
+export default Route.extend(ResetScrollMixin, {
   assetLoader: service(),
 
   beforeModel() {
