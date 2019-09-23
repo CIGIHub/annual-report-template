337301805b1b3db61ff4b9f6ead4d270a7af3af7
Gabriel Tan-Chen <gabriel@gabrieltanchen.com>
2019-09-23 11:47:03 -0400

    Initialize promise loop and s3 directory

--- migrate-images/index.js
+++ migrate-images/index.js
@@ -1,7 +1,10 @@
 require('dotenv').config();
 
 const AWS = require('aws-sdk');
+const crypto = require('crypto');
 const winston = require('winston');
+
+const nodes = require('../vendor/nodes');
 
 const logger = winston.createLogger({
   format: winston.format.simple(),
@@ -10,8 +13,17 @@ const logger = winston.createLogger({
     new winston.transports.Console(),
   ],
 });
+const s3 = new AWS.S3();
 
-// const s3 = new AWS.S3();
+let s3Directory = crypto.randomBytes(8).toString('hex');
+if (process.argv.slice(2)
+    && process.argv.slice(2)[0]
+    && typeof process.argv.slice(2)[0] === 'string'
+    && process.argv.slice(2)[0].length === 16) {
+  // If a directory was passed as an argument, use that.
+  s3Directory = process.argv.slice(2)[0];
+}
+
 // s3.putObject({
 //   Body: 'hello world',
 //   Bucket: 'annual-report-2019-static',
@@ -24,6 +36,22 @@ const logger = winston.createLogger({
 //   }
 // });
 
-Promise.resolve().then(() => {
+Promise.resolve().then(async() => {
   logger.info('Uploading nodes...');
+  const ASYNC_LIMIT = 10;
+  const nodeImages = {};
+
+  // Create offsets to upload node images in batches of ASYNC_LIMIT
+  const offsets = [];
+  for (let offset = 0; offset < nodes.length; offset += ASYNC_LIMIT) {
+    offsets.push(offset);
+  }
+
+  for (const offset of offsets) {
+    const batchNodes = nodes.slice(offset, offset + ASYNC_LIMIT);
+    const promises = [];
+    for (const node of batchNodes) {
+      logger.info(`Uploading node ${node.id}`);
+    }
+  }
 });
