f73e75edc73de945051a905d50323b4a1bc4c9c2
Gabriel Tan-Chen <gabriel@gabrieltanchen.com>
2019-09-26 18:18:49 -0400

    Update videos for Data Governance and Four Internets slides

--- app/controllers/exploring-the-four-internets-and-the-geopolitics-of-digital-governance.js
+++ app/controllers/exploring-the-four-internets-and-the-geopolitics-of-digital-governance.js
@@ -40,6 +40,6 @@ export default Controller.extend({
   }),
 
   videoLink: computed(function() {
-    return `${get(this, 'backgroundImage.imageHost')}4a7fcdebfbc4a053/slides/exploring-the-four-internets-and-the-geopolitics-of-digital-governance.mp4`;
+    return `${get(this, 'backgroundImage.imageHost')}ea0304a1ba15e9a1/slides/exploring-the-four-internets-and-the-geopolitics-of-digital-governance.mp4`;
   }),
 });
