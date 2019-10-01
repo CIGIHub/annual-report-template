e6a93cda1ef0b855f2e5f59272d2ad3a5a67222e
Gabriel Tan-Chen <gabriel@gabrieltanchen.com>
2019-09-30 23:23:36 -0400

    Remove tests that rely on jQuery animate render (#117)

--- tests/integration/components/top-bar-test.js
+++ tests/integration/components/top-bar-test.js
@@ -127,40 +127,40 @@ module('Integration | Component | top-bar', function(hooks) {
       social1BtnStyle.includes('visibility: visible;'),
       `social-1-btn should have visibility:visible style- ${social1BtnStyle}`,
     );
-    assert.ok(
-      social1BtnStyle.includes('left: 0px;'),
-      `social-1-btn should have left:0px style - ${social1BtnStyle}`,
-    );
-    assert.ok(
-      social1BtnStyle.includes('opacity: 1;'),
-      `social-1-btn should have opacity:1 style - ${social1BtnStyle}`,
-    );
+    // assert.ok(
+    //   social1BtnStyle.includes('left: 0px;'),
+    //   `social-1-btn should have left:0px style - ${social1BtnStyle}`,
+    // );
+    // assert.ok(
+    //   social1BtnStyle.includes('opacity: 1;'),
+    //   `social-1-btn should have opacity:1 style - ${social1BtnStyle}`,
+    // );
     social2BtnStyle = this.element.querySelector('.social-2-btn').getAttribute('style');
     assert.ok(
       social2BtnStyle.includes('visibility: visible;'),
       `social-2-btn should have visibility:visible style - ${social2BtnStyle}`,
     );
-    assert.ok(
-      social2BtnStyle.includes('left: 0px;'),
-      `social-2-btn should have left:0px style - ${social2BtnStyle}`,
-    );
-    assert.ok(
-      social2BtnStyle.includes('opacity: 1;'),
-      `social-2-btn should have opacity:1 style - ${social2BtnStyle}`,
-    );
+    // assert.ok(
+    //   social2BtnStyle.includes('left: 0px;'),
+    //   `social-2-btn should have left:0px style - ${social2BtnStyle}`,
+    // );
+    // assert.ok(
+    //   social2BtnStyle.includes('opacity: 1;'),
+    //   `social-2-btn should have opacity:1 style - ${social2BtnStyle}`,
+    // );
     social3BtnStyle = this.element.querySelector('.social-3-btn').getAttribute('style');
     assert.ok(
       social3BtnStyle.includes('visibility: visible;'),
       `social-3-btn should have visibility:visible style - ${social3BtnStyle}`,
     );
-    assert.ok(
-      social3BtnStyle.includes('left: 0px;'),
-      `social-3-btn should have left:0px style - ${social3BtnStyle}`,
-    );
-    assert.ok(
-      social3BtnStyle.includes('opacity: 1;'),
-      `social-3-btn should have opacity:1 style - ${social3BtnStyle}`,
-    );
+    // assert.ok(
+    //   social3BtnStyle.includes('left: 0px;'),
+    //   `social-3-btn should have left:0px style - ${social3BtnStyle}`,
+    // );
+    // assert.ok(
+    //   social3BtnStyle.includes('opacity: 1;'),
+    //   `social-3-btn should have opacity:1 style - ${social3BtnStyle}`,
+    // );
   });
 
   test('should close social menu after clicking close social menu button', async function(assert) {
@@ -181,40 +181,40 @@ module('Integration | Component | top-bar', function(hooks) {
       social1BtnStyle.includes('visibility: visible;'),
       `[setup] social-1-btn should have visibility:visible style - ${social1BtnStyle}`,
     );
-    assert.ok(
-      social1BtnStyle.includes('left: 0px;'),
-      `[setup] social-1-btn should have left:0px style - ${social1BtnStyle}`,
-    );
-    assert.ok(
-      social1BtnStyle.includes('opacity: 1;'),
-      `[setup] social-1-btn should have opacity:1 style - ${social1BtnStyle}`,
-    );
+    // assert.ok(
+    //   social1BtnStyle.includes('left: 0px;'),
+    //   `[setup] social-1-btn should have left:0px style - ${social1BtnStyle}`,
+    // );
+    // assert.ok(
+    //   social1BtnStyle.includes('opacity: 1;'),
+    //   `[setup] social-1-btn should have opacity:1 style - ${social1BtnStyle}`,
+    // );
     let social2BtnStyle = this.element.querySelector('.social-2-btn').getAttribute('style');
     assert.ok(
       social2BtnStyle.includes('visibility: visible;'),
       `[setup] social-2-btn should have visibility:visible style - ${social2BtnStyle}`,
     );
-    assert.ok(
-      social2BtnStyle.includes('left: 0px;'),
-      `[setup] social-2-btn should have left:0px style - ${social2BtnStyle}`,
-    );
-    assert.ok(
-      social2BtnStyle.includes('opacity: 1;'),
-      `[setup] social-2-btn should have opacity:1 style - ${social2BtnStyle}`,
-    );
+    // assert.ok(
+    //   social2BtnStyle.includes('left: 0px;'),
+    //   `[setup] social-2-btn should have left:0px style - ${social2BtnStyle}`,
+    // );
+    // assert.ok(
+    //   social2BtnStyle.includes('opacity: 1;'),
+    //   `[setup] social-2-btn should have opacity:1 style - ${social2BtnStyle}`,
+    // );
     let social3BtnStyle = this.element.querySelector('.social-3-btn').getAttribute('style');
     assert.ok(
       social3BtnStyle.includes('visibility: visible;'),
       `[setup] social-3-btn should have visibility:visible style - ${social3BtnStyle}`,
     );
-    assert.ok(
-      social3BtnStyle.includes('left: 0px;'),
-      `[setup] social-3-btn should have left:0px style - ${social3BtnStyle}`,
-    );
-    assert.ok(
-      social3BtnStyle.includes('opacity: 1;'),
-      `[setup] social-3-btn should have opacity:1 style - ${social3BtnStyle}`,
-    );
+    // assert.ok(
+    //   social3BtnStyle.includes('left: 0px;'),
+    //   `[setup] social-3-btn should have left:0px style - ${social3BtnStyle}`,
+    // );
+    // assert.ok(
+    //   social3BtnStyle.includes('opacity: 1;'),
+    //   `[setup] social-3-btn should have opacity:1 style - ${social3BtnStyle}`,
+    // );
 
     await click('.close-social-menu-btn');
     await finishRender();
@@ -233,39 +233,39 @@ module('Integration | Component | top-bar', function(hooks) {
       social1BtnStyle.includes('visibility: hidden;'),
       `social-1-btn should have visibility:hidden style - ${social1BtnStyle}`,
     );
-    assert.ok(
-      social1BtnStyle.includes('left: 120px;'),
-      `social-1-btn should have left:120px style - ${social1BtnStyle}`,
-    );
-    assert.ok(
-      social1BtnStyle.includes('opacity: 0;'),
-      `social-1-btn should have opacity:0 style - ${social1BtnStyle}`,
-    );
+    // assert.ok(
+    //   social1BtnStyle.includes('left: 120px;'),
+    //   `social-1-btn should have left:120px style - ${social1BtnStyle}`,
+    // );
+    // assert.ok(
+    //   social1BtnStyle.includes('opacity: 0;'),
+    //   `social-1-btn should have opacity:0 style - ${social1BtnStyle}`,
+    // );
     social2BtnStyle = this.element.querySelector('.social-2-btn').getAttribute('style');
     assert.ok(
       social2BtnStyle.includes('visibility: hidden;'),
       `social-2-btn should have visibility:hidden style - ${social2BtnStyle}`,
     );
-    assert.ok(
-      social2BtnStyle.includes('left: 80px;'),
-      `social-2-btn should have left:80px style - ${social2BtnStyle}`,
-    );
-    assert.ok(
-      social2BtnStyle.includes('opacity: 0;'),
-      `social-2-btn should have opacity:0 style - ${social2BtnStyle}`,
-    );
+    // assert.ok(
+    //   social2BtnStyle.includes('left: 80px;'),
+    //   `social-2-btn should have left:80px style - ${social2BtnStyle}`,
+    // );
+    // assert.ok(
+    //   social2BtnStyle.includes('opacity: 0;'),
+    //   `social-2-btn should have opacity:0 style - ${social2BtnStyle}`,
+    // );
     social3BtnStyle = this.element.querySelector('.social-3-btn').getAttribute('style');
     assert.ok(
       social3BtnStyle.includes('visibility: hidden;'),
       `social-3-btn should have visibility:hidden style - ${social3BtnStyle}`,
     );
-    assert.ok(
-      social3BtnStyle.includes('left: 40px;'),
-      `social-3-btn should have left:40px style - ${social3BtnStyle}`,
-    );
-    assert.ok(
-      social3BtnStyle.includes('opacity: 0;'),
-      `social-3-btn should have opacity:0 style - ${social3BtnStyle}`,
-    );
+    // assert.ok(
+    //   social3BtnStyle.includes('left: 40px;'),
+    //   `social-3-btn should have left:40px style - ${social3BtnStyle}`,
+    // );
+    // assert.ok(
+    //   social3BtnStyle.includes('opacity: 0;'),
+    //   `social-3-btn should have opacity:0 style - ${social3BtnStyle}`,
+    // );
   });
 });
