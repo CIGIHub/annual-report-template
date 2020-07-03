0d09e8e27a56025882cf6555a1fbef720efdce0e
Gabriel Tan-Chen <gabriel@gabrieltanchen.com>
2020-07-03 14:13:45 -0400

    convert video-background component

--- app/components/video-background.js
+++ app/components/video-background.js
@@ -2,8 +2,6 @@ import ENV from 'annual-report-template/config/environment';
 import Component from '@ember/component';
 
 export default Component.extend({
-  videoLink: '',
-
   didInsertElement() {
     const video = document.getElementById('background-video');
     if (video && ENV.environment !== 'test') {
