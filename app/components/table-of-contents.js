ca120d09207cdce60cc8edb8eafd7c216254d886
Gabriel Tan-Chen <gabriel@gabrieltanchen.com>
2020-07-06 12:50:57 -0400

    Fix Promise and missing padStart errors on IE (#403)
    * Fix Promise and missing padStart errors on IE
    
    * fix tests

--- app/components/table-of-contents.js
+++ app/components/table-of-contents.js
@@ -16,9 +16,9 @@ export default Component.extend({
     }
     const currentRouteName = this.router.currentRouteName;
     const links = this.routes.slice(0, this.linksBreakAtIndex);
-    return links.map((link, index) => ({
+    return links.map((link) => ({
       current: link.route === currentRouteName,
-      number: (index + 1).toString().padStart(2, '0'),
+      number: link.number,
       route: link.route,
       title: this.intl.t(link.title),
     }));
@@ -30,9 +30,9 @@ export default Component.extend({
     }
     const currentRouteName = this.router.currentRouteName;
     const links = this.routes.slice(this.linksBreakAtIndex);
-    return links.map((link, index) => ({
+    return links.map((link) => ({
       current: link.route === currentRouteName,
-      number: (index + this.linksBreakAtIndex + 1).toString().padStart(2, '0'),
+      number: link.number,
       route: link.route,
       title: this.intl.t(link.title),
     }));
--- app/services/route-order.js
+++ app/services/route-order.js
@@ -3,82 +3,102 @@ import Service from '@ember/service';
 
 export default Service.extend({
   routes: [{
+    number: '00',
     route: 'index',
     subRoutes: [],
     title: 'home',
   }, {
+    number: '00',
     route: 'table-of-contents',
     subRoutes: [],
     title: 'tableOfContents.title',
   }, {
+    number: '01',
     route: 'chairs-message',
     subRoutes: [],
     title: 'chairsMessage.title',
   }, {
+    number: '02',
     route: 'presidents-message',
     subRoutes: [],
     title: 'presidentsMessage.title',
   }, {
+    number: '03',
     route: 'building-closer-ties-between-canada-and-india',
     subRoutes: [],
     title: 'buildingCloserTiesBetweenCanadaAndIndia.title',
   }, {
+    number: '04',
     route: 'a-clarion-call-for-protecting-the-worlds-refugees',
     subRoutes: [],
     title: 'aClarionCallForProtectingTheWorldsRefugees.title',
   }, {
+    number: '05',
     route: 'multilateral-institutions-mark-a-tumultuous-year',
     subRoutes: [],
     title: 'multilateralInstitutionsMarkATumultuousYear.title',
   }, {
+    number: '06',
     route: 'governing-cyberspace-during-a-crisis-in-trust',
     subRoutes: [],
     title: 'governingCyberspaceDuringACrisisInTrust.title',
   }, {
+    number: '07',
     route: 'governing-the-digital-public-sphere',
     subRoutes: [],
     title: 'governingTheDigitalPublicSphere.title',
   }, {
+    number: '08',
     route: 'media-and-mass-atrocity-the-rwanda-genocide-and-beyond',
     subRoutes: [],
     title: 'mediaAndMassAtrocityTheRwandaGenocideAndBeyond.title',
   }, {
+    number: '09',
     route: 'braiding-legal-orders',
     subRoutes: [],
     title: 'braidingLegalOrders.title',
   }, {
+    number: '10',
     route: 'models-for-data-governance',
     subRoutes: [],
     title: 'modelsForDataGovernance.title',
   }, {
+    number: '11',
     route: 'protecting-elections-from-foreign-interference',
     subRoutes: [],
     title: 'protectingElectionsFromForeignInterference.title',
   }, {
+    number: '12',
     route: 'a-bretton-woods-moment-for-the-digital-age',
     subRoutes: [],
     title: 'aBrettonWoodsMomentForTheDigitalAge.title',
   }, {
+    number: '13',
     route: 'informing-the-debate-around-internet-security-and-trust',
     subRoutes: [],
     title: 'informingTheDebateAroundInternetSecurityAndTrust.title',
   }, {
+    number: '14',
     route: 'exploring-the-four-internets-and-the-geopolitics-of-digital-governance',
     subRoutes: [],
     title: 'exploringTheFourInternetsAndTheGeopoliticsOfDigitalGovernance.title',
   }, {
+    number: '15',
     route: 'curbing-cultural-appropriation-through-intellectual-property-law',
     subRoutes: [],
     title: 'curbingCulturalAppropriationThroughIntellectualPropertyLaw.title',
   }, {
+    number: '16',
     route: 'outputs-and-activities',
     subRoutes: [],
     title: 'outputsAndActivities.title',
   }, {
+    number: '17',
     route: 'timeline',
     subRoutes: [],
     title: 'timeline.title',
   }, {
+    number: '18',
     route: 'financials.auditors-report',
     subRoutes: [
       'financials.balance-sheet',
@@ -87,6 +107,7 @@ export default Service.extend({
     ],
     title: 'financials.title',
   }, {
+    number: '19',
     route: 'thank-you',
     subRoutes: [],
     title: 'thankYou.title',
