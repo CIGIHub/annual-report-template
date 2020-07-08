352adabf1cc6ef9c176cf028412601ca778145c9
Gabriel Tan-Chen <gabriel@gabrieltanchen.com>
2020-07-08 15:44:02 -0400

    Update slides to include generic content (#416)
    * slide 1
    
    * slide 2
    
    * fix slide tests
    
    * slide 3
    
    * slide 4
    
    * slide 5
    
    * slide 6
    
    * slide 7
    
    * slide 8
    
    * slide 9
    
    * slide 10
    
    * slide 11
    
    * slide 12
    
    * slide 13
    
    * more acceptance tests
    
    * slide 1 test
    
    * slide 2 tests
    
    * slide 3 tests
    
    * slide 4 tests
    
    * slide 5 tests
    
    * slide 6 tests
    
    * slide 7 tests
    
    * slide 8 tests
    
    * slide 9 tests
    
    * slide 10 tests
    
    * slide 11 tests
    
    * slide 12 tests
    
    * slide 13 tests
    
    * outputs and activities tests
    
    * timeline tests
    
    * updated financials
    
    * financials tests
    
    * thank you tests
    
    * fix tests

--- tests/acceptance/financials/notes-test.js
+++ tests/acceptance/financials/notes-test.js
@@ -12,36 +12,59 @@ module('Acceptance | financials/notes', function(hooks) {
     assert.equal(currentURL(), '/financials/notes');
   });
 
-  test('should have correct head title', async function(assert) {
+  test('should have correct meta', async function(assert) {
     await visit('/financials/notes');
+
     assert.strictEqual(
       document.querySelector('head meta[property="og:title"]').getAttribute('content'),
       'Financials | 2019 CIGI Annual Report',
     );
-  });
-
-  test('should have correct head description', async function(assert) {
-    await visit('/financials/notes');
     assert.strictEqual(
       document.querySelector('head meta[property="og:description"]').getAttribute('content'),
       'Explore a year of research, opinion and expertise in CIGI’s interactive annual report.',
     );
-  });
-
-  test('should have correct head site name', async function(assert) {
-    await visit('/financials/notes');
     assert.strictEqual(
       document.querySelector('head meta[property="og:site_name"]').getAttribute('content'),
       '2019 CIGI Annual Report',
     );
-  });
-
-  test('should have correct head url', async function(assert) {
-    await visit('/financials/notes');
     assert.strictEqual(
       document.querySelector('head meta[property="og:url"]').getAttribute('content'),
       'https://www.cigionline.org/interactives/2019annualreport/financials/notes/',
     );
+  });
+
+  test('should have correct social links', async function(assert) {
+    await visit('/financials/notes');
+
+    assert.dom('.social-2-btn').hasAttribute('href', 'https://twitter.com/intent/tweet?status=2019+CIGI+Annual+Report+https://www.cigionline.org/interactives/2019annualreport/financials/notes');
+    assert.dom('.social-3-btn').hasAttribute('href', 'https://www.linkedin.com/shareArticle?mini=true&url=https://www.cigionline.org/interactives/2019annualreport/financials/notes');
+  });
+
+  test('should have correct slide content', async function(assert) {
+    await visit('/financials/notes');
+
+    assert.dom('.financials').exists();
+    assert.dom('.financials h1').containsText('Financials');
+    assert.dom('.financials .financials-menu').exists();
+    assert.dom('.financials .financials-menu').containsText('Notes');
+    assert.dom('.financials .financials-menu > a').exists({ count: 3 });
+    assert.dom('.financials .financials-menu > a:nth-of-type(1)').containsText('Auditor’s Report');
+    assert.dom('.financials .financials-menu > a:nth-of-type(2)').containsText('Summarized Statement of Financial Position');
+    assert.dom('.financials .financials-menu > a:nth-of-type(3)').containsText('Summarized Statement of Revenues and Expenditures and Changes in Fund Balances');
+    assert.dom('.financials .financials-menu .download-button').exists();
+    assert.dom('.financials .financials-menu .download-button a').containsText('Download PDF');
+    assert.dom('.financials .financials-content').exists();
+    assert.dom('.financials .financials-content h4').containsText('Notes to the Summary Financial Statements');
+
+    assert.dom('.cigi-top-bar').exists();
+    assert.dom('.cigi-top-bar').hasClass('light-background');
+    assert.dom('ul.dot-nav').exists();
+    assert.dom('ul.dot-nav').hasClass('light-background');
+    assert.dom('button.scroll-arrow-up-btn').exists();
+    assert.dom('button.scroll-arrow-up-btn').hasClass('light-background');
+    assert.dom('button.scroll-arrow-down-btn').exists();
+    assert.dom('button.scroll-arrow-down-btn').hasClass('light-background');
+    assert.dom('.footer').doesNotExist();
   });
 
   test('should transition to /timeline on scroll-arrow-up-btn click', async function(assert) {
@@ -55,6 +78,7 @@ module('Acceptance | financials/notes', function(hooks) {
     await finishRender();
 
     assert.strictEqual(currentURL(), '/timeline');
+    assert.dom('ul.dot-nav').exists();
   });
 
   test('should transition to /thank-you on scroll-arrow-down-btn click', async function(assert) {
@@ -68,5 +92,6 @@ module('Acceptance | financials/notes', function(hooks) {
     await finishRender();
 
     assert.strictEqual(currentURL(), '/thank-you');
+    assert.dom('ul.dot-nav').exists();
   });
 });
